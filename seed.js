0a8a110c048f56a068e78b5a4ce45908c123dab2
AndrewTrahan <globalART19@gmail.com>
2018-04-30 17:50:36 -0500

    Andrew: going home...basically no change from end of class

--- seed.js
+++ seed.js
@@ -1,40 +1,61 @@
-// const Express = require('express')
-// const app = new Express()
-// const morgan = require('morgan')
 const { db, Vegetable, Gardener, Plot } = require('./models')
 
-// app.use(morgan())
 const veggies = ['carrot', 'tomato', 'bean', 'corn', 'squash', 'lechuga'];
 const gardeners = ['gary', 'steve', 'joe'];
 const plots = [7, 4, 10];
 
 const PORT = 1337
 
-function seed(vegArr, gardArr, plotArr) {
-  for (let i = 0; i < veggies.length; i++) {
-    console.log(veggies[i]);
-    Vegetable.create({ name: veggies[i] })
+function seed() {
+  let i = 0
+  return Promise.all(veggies.map((veg) => {
+    console.log(veg);
+    Vegetable.create({ name: veg })
       .then((veggie) => {
-        console.log('inserted')
-        return Gardener.create({ name: gardeners[i] })
+        return Gardener.create({ name: gardeners[i], favoriteVegetableId: veggie.id })
       })
-      .then((gardner) => {
-        console.log('inserted')
-        return Plot.create({ size: plots[i] })
+      .then((gardener) => {
+        console.log('inserted gardener')
+        return Plot.create({ size: plots[i], gardenerId: gardener.id })
       })
+      .then(() => { console.log('plot inserted') })
       .catch((error) => {
-        console.error('no more gardeners/plots', error);
+        console.error('no more gardeners/plots');
+        return null
       })
-  }
+    i++
+  }))
+}
+
+function seed2(vegArr) {
+  let i = 0
+  return Promise.all(vegArr.map((veg) => {
+    console.log(veg);
+    Vegetable.create({ name: veg })
+      .then((veggie) => {
+        return Gardener.create({ name: gardeners[i], favoriteVegetableId: veggie.id })
+      })
+      .then((gardener) => {
+        console.log('inserted gardener')
+        return Plot.create({ size: plots[i], gardenerId: gardener.id })
+      })
+      .then(() => { console.log('plot inserted') })
+      .catch((error) => {
+        console.error('no more gardeners/plots');
+        return null
+      })
+    i++
+  }))
 }
 
 function init() {
   db.sync({ force: true, })
     .then(() => {
       console.log('good job it worked!!')
-      return seed(veggies, gardeners, plots)
+      let i = 0
+      return seed()
     })
-    //.then(() => { db.close() })
+    // .then(() => { db.close() })
     .catch((err) => {
       console.error('Oops,  you broke it!', err)
     })
